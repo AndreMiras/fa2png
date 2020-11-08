3878c6c9686c6fbfa889c42a0f8c3ad0ffbbc00f
Andre Miras <andre.miras@gmail.com>
2020-11-08 19:10:59 +0100

    :rotating_light: yarn format

--- src/components/Container.js
+++ src/components/Container.js
@@ -13,14 +13,14 @@ const Container = () => (
               <div className="input-group">
                 <span className="input-group-addon left">Icon</span>
                 <input data-placement="bottomRight" className="form-control iconpicker" value="fa-paw" type="text" />
-                <span className="input-group-addon picker-component-selector right"></span>
+                <span className="input-group-addon picker-component-selector right" />
               </div>
             </div>
             <div className="form-group">
               <div className="input-group colorpicker-component">
                 <span className="input-group-addon left">Color</span>
                 <input className="form-control" value="#333333" type="text" />
-                <span className="input-group-addon picker-component-selector right"><i></i></span>
+                <span className="input-group-addon picker-component-selector right"><i /></span>
               </div>
             </div>
             <div className="form-group">
@@ -29,17 +29,25 @@ const Container = () => (
                 <input id="font-input" className="form-control" value="15" type="text" />
                 <span className="input-group-addon right">px</span>
               </div>
-              <div className="size-slider" style={{ width: "100%" }}></div>
+              <div className="size-slider" style={{ width: '100%' }} />
             </div>
             <div className="btn-group">
-                <button type="button" className="btn btn-default" onclick="capture()"><FontAwesomeIcon icon="magic" /> Capture</button>
+              <button type="button" className="btn btn-default" onClick="capture()">
+                <FontAwesomeIcon icon="magic" />
+                {' '}
+                Capture
+              </button>
               <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span className="caret"></span>
+                <span className="caret" />
                 <span className="sr-only">Toggle Dropdown</span>
               </button>
               <ul className="dropdown-menu">
                 <li>
-                  <a href="#" onclick="capture_and_download()"><FontAwesomeIcon icon="download" /> Capture & Download</a>
+                  <a href="#" onClick="capture_and_download()">
+                    <FontAwesomeIcon icon="download" />
+                    {' '}
+                    Capture & Download
+                  </a>
                 </li>
               </ul>
             </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,9 @@ const Nav = () => (
       <div className="navbar-header">
         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
           <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
         </button>
         <a className="navbar-brand" href="#">FontAwesome to PNG</a>
       </div>
