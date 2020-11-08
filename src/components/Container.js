9244c9339bb87ee019337f7f015a1c3cacee382c
Andre Miras <andre.miras@gmail.com>
2020-11-08 17:39:11 +0100

    :construction: Nav and Container components, refs #4

---
+++ src/components/Container.js
@@ -0,0 +1,63 @@
+const Container = () => (
+  <div className="container theme-showcase" role="main">
+    <div className="row">
+      <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Configure icon</h3>
+          </div>
+          <div className="panel-body">
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon left">Icon</span>
+                <input data-placement="bottomRight" className="form-control iconpicker" value="fa-paw" type="text" />
+                <span className="input-group-addon picker-component-selector right"></span>
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-group colorpicker-component">
+                <span className="input-group-addon left">Color</span>
+                <input className="form-control" value="#333333" type="text" />
+                <span className="input-group-addon picker-component-selector right"><i></i></span>
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon left">Size</span>
+                <input id="font-input" className="form-control" value="15" type="text" />
+                <span className="input-group-addon right">px</span>
+              </div>
+              <div className="size-slider" style={{ width: "100%" }}></div>
+            </div>
+            <div className="btn-group">
+                <button type="button" className="btn btn-default" onclick="capture()"><i className="fa fa-magic"></i> Capture</button>
+              <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span className="caret"></span>
+                <span className="sr-only">Toggle Dropdown</span>
+              </button>
+              <ul className="dropdown-menu">
+                <li>
+                  <a href="#" onclick="capture_and_download()"><i className="fa fa-download"></i> Capture & Download</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-9">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Icon preview</h3>
+          </div>
+          <div className="panel-body">
+            <div className="text-center" id="icon-canvas">
+              <i id="icon-target" className="fa fa-paw fa-5x"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Container;
---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+const Nav = () => (
+  <nav className="navbar navbar-inverse navbar-fixed-top">
+    <div className="container">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="#">FontAwesome to PNG</a>
+      </div>
+      <div id="navbar" className="navbar-collapse collapse">
+        <ul className="nav navbar-nav">
+          <li className="active"><a href="/">Home</a></li>
+          <li><a href="https://github.com/AndreMiras/fa2png">About</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Nav;
