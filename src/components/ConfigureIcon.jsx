aa6fe5a4de3d9a227cfbd53dade8747f61822898
Andre Miras <andre.miras@gmail.com>
2020-11-08 20:22:56 +0100

    :recycle: New ConfigureIcon dedicated component

---
+++ src/components/ConfigureIcon.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Button, Dropdown, MenuItem } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const capture = () => (
+  console.log('capture()')
+);
+
+const captureAndDownload = () => (
+  console.log('captureAndDownload()')
+);
+
+const ConfigureIcon = () => (
+  <div className="panel panel-default">
+    <div className="panel-heading">
+      <h3 className="panel-title">Configure icon</h3>
+    </div>
+    <div className="panel-body">
+      <div className="form-group">
+        <div className="input-group">
+          <span className="input-group-addon left">Icon</span>
+          <input data-placement="bottomRight" className="form-control iconpicker" value="fa-paw" type="text" />
+          <span className="input-group-addon picker-component-selector right" />
+        </div>
+      </div>
+      <div className="form-group">
+        <div className="input-group colorpicker-component">
+          <span className="input-group-addon left">Color</span>
+          <input className="form-control" value="#333333" type="text" />
+          <span className="input-group-addon picker-component-selector right"><i /></span>
+        </div>
+      </div>
+      <div className="form-group">
+        <div className="input-group">
+          <span className="input-group-addon left">Size</span>
+          <input id="font-input" className="form-control" value="15" type="text" />
+          <span className="input-group-addon right">px</span>
+        </div>
+        <div className="size-slider" style={{ width: '100%' }} />
+      </div>
+      <Dropdown>
+        <Button onClick={capture}>
+          <FontAwesomeIcon icon="magic" />
+          {' '}
+          Capture
+        </Button>
+        <Dropdown.Toggle />
+        <Dropdown.Menu className="super-colors">
+          <MenuItem eventKey="1">
+            <Button bsStyle="link" onClick={captureAndDownload}>
+              <FontAwesomeIcon icon="download" />
+              {' '}
+              Capture & Download
+            </Button>
+          </MenuItem>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  </div>
+);
+
+export default ConfigureIcon;
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,66 +1,15 @@
 import React from 'react';
-import { Button, Dropdown, MenuItem } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-const capture = () => (
-  console.log('capture()')
-);
-
-const captureAndDownload = () => (
-  console.log('captureAndDownload()')
-);
+import ConfigureIcon from './ConfigureIcon';
 
 const Container = () => (
   <div className="container theme-showcase" role="main">
     <div className="row">
+
       <div className="col-md-3">
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <h3 className="panel-title">Configure icon</h3>
-          </div>
-          <div className="panel-body">
-            <div className="form-group">
-              <div className="input-group">
-                <span className="input-group-addon left">Icon</span>
-                <input data-placement="bottomRight" className="form-control iconpicker" value="fa-paw" type="text" />
-                <span className="input-group-addon picker-component-selector right" />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="input-group colorpicker-component">
-                <span className="input-group-addon left">Color</span>
-                <input className="form-control" value="#333333" type="text" />
-                <span className="input-group-addon picker-component-selector right"><i /></span>
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="input-group">
-                <span className="input-group-addon left">Size</span>
-                <input id="font-input" className="form-control" value="15" type="text" />
-                <span className="input-group-addon right">px</span>
-              </div>
-              <div className="size-slider" style={{ width: '100%' }} />
-            </div>
-            <Dropdown>
-              <Button onClick={capture}>
-                <FontAwesomeIcon icon="magic" />
-                {' '}
-                Capture
-              </Button>
-              <Dropdown.Toggle />
-              <Dropdown.Menu className="super-colors">
-                <MenuItem eventKey="1">
-                  <Button bsStyle="link" onClick={captureAndDownload}>
-                    <FontAwesomeIcon icon="download" />
-                    {' '}
-                    Capture & Download
-                  </Button>
-                </MenuItem>
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </div>
+        <ConfigureIcon />
       </div>
+
       <div className="col-md-9">
         <div className="panel panel-default">
           <div className="panel-heading">
