4f6169cf9131ea2fde060259083bef9b05351c73
Andre Miras <andre.miras@gmail.com>
2020-11-09 15:16:31 +0100

    :construction_worker: WIP Color picker
    Note the tooltip is not rendering where it should.

--- src/components/ColorInput.jsx
+++ src/components/ColorInput.jsx
@@ -2,7 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { SketchPicker } from 'react-color';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
 import CustomInput from './CustomInput';
+
+const CustomPopover = ({ onChange, value }) => (
+  <Popover>
+    <Popover.Title as="h3">Color</Popover.Title>
+    <Popover.Content>
+      <SketchPicker color={value.substring(1)} onChange={(color) => onChange(color.hex)} />
+    </Popover.Content>
+  </Popover>
+);
+CustomPopover.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+const CustomOverlayTrigger = ({ onChange, value }) => (
+  <OverlayTrigger
+    trigger="click"
+    placement="right"
+    overlay={<CustomPopover onChange={onChange} value={value} />}
+  >
+    <FontAwesomeIcon icon="palette" />
+  </OverlayTrigger>
+);
+CustomOverlayTrigger.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 const ColorInput = ({ onChange, value }) => (
   <>
@@ -10,9 +38,8 @@ const ColorInput = ({ onChange, value }) => (
       onChange={onChange}
       prepend="Color"
       value={value}
-      append={<FontAwesomeIcon icon="palette" />}
+      append={<CustomOverlayTrigger onChange={onChange} value={value} />}
     />
-    <SketchPicker color={value.substring(1)} onChange={(color) => onChange(color.hex)} />
   </>
 );
 ColorInput.propTypes = {
