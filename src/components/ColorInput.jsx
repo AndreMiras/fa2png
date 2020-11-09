050ef2ceeb8025c2583e891d56295ecbdfb98f26
Andre Miras <andre.miras@gmail.com>
2020-11-09 14:48:30 +0100

    :sparkles: New color picker

--- src/components/ColorInput.jsx
+++ src/components/ColorInput.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SketchPicker } from 'react-color';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CustomInput from './CustomInput';
 
 const ColorInput = ({ onChange, value }) => (
-  <CustomInput onChange={onChange} prepend="Color" value={value} append={<FontAwesomeIcon icon="palette" />} />
+  <>
+    <CustomInput
+      onChange={onChange}
+      prepend="Color"
+      value={value}
+      append={<FontAwesomeIcon icon="palette" />}
+    />
+    <SketchPicker color={value.substring(1)} onChange={(color) => onChange(color.hex)} />
+  </>
 );
 ColorInput.propTypes = {
   onChange: PropTypes.func.isRequired,
