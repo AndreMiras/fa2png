c17767724ce46c508f9652b0bed4a77a37545db4
Andre Miras <andre.miras@gmail.com>
2020-11-09 14:35:02 +0100

    :recycle: Dedicated SizeInputWithSlider component

--- src/components/ConfigureIcon.jsx
+++ src/components/ConfigureIcon.jsx
@@ -1,44 +1,11 @@
 import React from 'react';
 import './ConfigureIcon.css';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 import PropTypes from 'prop-types';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import CaptureButton from './CaptureButton';
-import CustomInput from './CustomInput';
 import IconInput from './IconInput';
 import ColorInput from './ColorInput';
-
-const SizeInput = ({ onChange, value }) => (
-  <CustomInput onChange={onChange} prepend="Size" value={value.toString()} append="px" />
-);
-SizeInput.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-const CustomSlider = ({ onChange, value }) => (
-  <Row>
-    <Col className="mb-3">
-      <Slider onChange={onChange} value={value} max={500} />
-    </Col>
-  </Row>
-);
-CustomSlider.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-const SizeInputWithSlider = ({ onChange, value }) => (
-  <>
-    <SizeInput onChange={onChange} value={value} />
-    <CustomSlider onChange={onChange} value={value} />
-  </>
-);
-SizeInputWithSlider.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.number.isRequired,
-};
+import SizeInputWithSlider from './SizeInputWithSlider';
 
 const ConfigureIcon = ({
   onIconChange, iconValue, onColorChange, colorValue, onSizeChange, sizeValue,
---
+++ src/components/SizeInputWithSlider.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import PropTypes from 'prop-types';
+import { Col, Row } from 'react-bootstrap';
+import CustomInput from './CustomInput';
+
+const SizeInput = ({ onChange, value }) => (
+  <CustomInput onChange={onChange} prepend="Size" value={value.toString()} append="px" />
+);
+SizeInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+const CustomSlider = ({ onChange, value }) => (
+  <Row>
+    <Col className="mb-3">
+      <Slider onChange={onChange} value={value} max={500} />
+    </Col>
+  </Row>
+);
+CustomSlider.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+const SizeInputWithSlider = ({ onChange, value }) => (
+  <>
+    <SizeInput onChange={onChange} value={value} />
+    <CustomSlider onChange={onChange} value={value} />
+  </>
+);
+SizeInputWithSlider.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export default SizeInputWithSlider;
