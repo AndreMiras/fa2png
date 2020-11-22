795d61bc712bf32bec4afe1a0b6e6f27b0f2b00a
Andre Miras <andre.miras@gmail.com>
2020-11-22 22:51:54 +0100

    :iphone: Expands navbar on small, refs #11

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Container, Nav as ReactNav, Navbar } from 'react-bootstrap';
 
 const Nav = () => (
-  <Navbar bg="dark" variant="dark" className="mb-sm-4">
+  <Navbar bg="dark" variant="dark" expand="sm" className="mb-4">
     <Container>
       <Navbar.Brand href={process.env.PUBLIC_URL}>FontAwesome to PNG</Navbar.Brand>
-      <ReactNav className="mr-auto">
-        <ReactNav.Link href={process.env.PUBLIC_URL}>Home</ReactNav.Link>
-        <ReactNav.Link href="https://github.com/AndreMiras/fa2png">About</ReactNav.Link>
-      </ReactNav>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <ReactNav className="mr-auto">
+          <ReactNav.Link href={process.env.PUBLIC_URL}>Home</ReactNav.Link>
+          <ReactNav.Link href="https://github.com/AndreMiras/fa2png">About</ReactNav.Link>
+        </ReactNav>
+      </Navbar.Collapse>
     </Container>
   </Navbar>
 );
